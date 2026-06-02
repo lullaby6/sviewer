d68d70f1f5794d73d33cfa7642e937054040ebb4
lullaby6 <lucianobrumer5@gmail.com>
2026-06-02 10:58:56 -0300

    vite base

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.VITE_BASE ?? '/',
   server: {
     port: 3000
   },
